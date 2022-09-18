0dd3551d0592fbd2773b8575fbff46ae7c029396
Ian Ellis <iellis@minevaluation.com>
2022-09-18 13:30:03 -0600

    CTCI 16.15: Master Mind added

---
+++ CrackingTheCodingInterview/Ch8/8.7-string_permutations_unique.py
@@ -0,0 +1,19 @@
+def string_permutations(s: str) -> list:
+    """Find all the permutations of a string of unique characters"""
+    perms = ['']
+    for character in s:
+        bases = perms
+        perms = []
+        for base in bases:
+            for i in range(len(base) + 1):
+                string = base[:i] + character + base[i:]
+                perms.append(string)
+
+    return perms
+
+
+if __name__ == '__main__':
+    test_str = 'abcd'
+    permutations = string_permutations(test_str)
+    print(len(permutations))
+    print(permutations)
