b9e9b15e1b11ce2db534ce46ce831504d0c63e99
Ian Ellis <iellis@minevaluation.com>
2023-08-23 17:34:33 -0600

    Updated LeetCode problem 983

--- LeetCode/Minimum_Cost_For_Tickets.py
+++ LeetCode/Minimum_Cost_For_Tickets.py
@@ -18,18 +18,25 @@ import bisect
 
 class Solution:
     def mincostTickets(self, days: list[int], costs: list[int]) -> int:
-        pass_days = [1, 7, 30]
-        table = [0]*(len(days)+1)
-        for i, day in enumerate(days):
-            candidates = []
-            for p, c in zip(pass_days, costs):
-                j = bisect.bisect(days, day-p, 0, i)
-                candidates.append(table[j]+c)
-            table[i+1] = min(candidates)
-        return table[-1]
+      price = [0] * days[-1]
+      days_set = set(days)
+      
+      for day in range(days[-1]):
+        if day+1 in days_set:
+          d1 = price[day - 1] + costs[0] if day - 1 >= 0 else costs[0]
+          d7 = price[day - 7] + costs[1] if day - 7 >= 0 else costs[1]
+          d30 = price[day - 30] + costs[2] if day - 30 >= 0 else costs[2]
+          price[day] = min(d1, d7, d30)
+        else:
+          price[day] = price[day-1]
+
+      return price[-1]
     
-if __name__ == "__main__":
-    days = [1,2,3,4,5,6,7,8,9,10,30,31]
-    costs = [2,7,15]
-    solver = Solution()
-    print(solver.mincostTickets(days, costs))
+
+solver = Solution()
+days = [1,4,6,7,8,20]
+costs = [2,7,15]
+print(solver.mincostTickets(days, costs))
+days = [1,2,3,4,5,6,7,8,9,10,30,31]
+costs = [2,7,15]
+print(solver.mincostTickets(days, costs))
